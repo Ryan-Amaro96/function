6c985d368f7662025c02a9bc09d1fba876287032
Ryan Amaro <ryanamaro2003.st@gmail.com>
2022-06-20 01:02:12 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function sms(){
+    console.log('Estou aprendendo function')
+}
+let messagem=sms()
+function nome(Ryan){
+    console.log(`Oi meu nome é ${Ryan}`)
+}
+let nome=nome(Ryan)
+function nasm(Ryan,dezenove,rap){
+    console.log(`Oi meu nome é ${Ryan} tenho ${dezenove}anos e meu estilo musical preferido é ${rap}`)
+}
+nasm(Ryan,dezenove,rap)
+function movieMusic(vingadores,hoodfellas){
+    console.log(`minha música preferida é ${hoodfellas} e meu filme preferido é ${vingadores}`)
+}
+function triple(number){
+    return number*3
+}
+console.log(triple(3))
